b9be4eb116dfc0902a3c6c82e92142d6074c49f6
isra <isragonher@gmail.com>
2022-07-01 12:15:24 +0200

    deploy whitout json server

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,66 +1,81 @@
-import { createContext, useState, useEffect } from 'react'
+import { createContext, useState, /*useEffect*/ } from 'react'
+import { v4 as uuidv4 } from 'uuid'
+
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({ children }) => {
-    const [isLoading, setIsLoading] = useState(true)
-    const [feedback, setFeedback] = useState([])
+    // const [isLoading, setIsLoading] = useState(true)
+    const [feedback, setFeedback] = useState([
+        {
+            id: 1,
+            rating: 10,
+            text: "This is feedback item 1 coming from the backend"
+        },
+        {
+            id: 2,
+            rating: 8,
+            text: "This is feedback item 2 coming from the backend"
+        }
+    ])
     const [feedbackEdit, setFeedbackEdit] = useState({
         item: {},
         edit: false,
     })
 
-    useEffect(() => {
-        fetchFeedback()
-    }, [])
+    // useEffect(() => {
+    //     fetchFeedback()
+    // }, [])
 
     // Fetch feedback
-    const fetchFeedback = async () => {
-        const response = await fetch(`http://localhost:5000/feedback?_sort=id&_order=desc`)
-        const data = await response.json()
+    // const fetchFeedback = async () => {
+    //     const response = await fetch(`http://localhost:5000/feedback?_sort=id&_order=desc`)
+    //     const data = await response.json()
 
-        setFeedback(data)
-        setIsLoading(false)
-    }
+    //     setFeedback(data)
+    //     setIsLoading(false)
+    // }
 
     // Add feedback
-    const addFeedback = async (newFeedback) => {
-        const response = await fetch('http://localhost:5000/feedback', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(newFeedback),
-        })
+    const addFeedback = /*async*/ (newFeedback) => {
+        // const response = await fetch('http://localhost:5000/feedback', {
+        //     method: 'POST',
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //     },
+        //     body: JSON.stringify(newFeedback),
+        // })
 
-        const data = await response.json()
+        // const data = await response.json()
 
-        setFeedback([data, ...feedback])
+        // setFeedback([data, ...feedback])
+        newFeedback.id = feedback.uuid
+        setFeedback([newFeedback, ...feedback])
     }
 
     // Delete feedback
     const deleteFeedback = async (id) => {
         if (window.confirm('Are you sure you want to delete?')) {
-            await fetch(`http://localhost:5000/feedback/${id}`, { method: 'DELETE' })
+            // await fetch(`http://localhost:5000/feedback/${id}`, { method: 'DELETE' })
 
             setFeedback(feedback.filter((item) => item.id !== id))
         }
     }
 
     // Update feedback item
-    const updateFeedback = async (id, updItem) => {
-        const response = await fetch(`http://localhost:5000/feedback/${id}`, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(updItem),
-        })
+    const updateFeedback = /*async*/ (id, updItem) => {
+        // const response = await fetch(`http://localhost:5000/feedback/${id}`, {
+        //     method: 'PUT',
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //     },
+        //     body: JSON.stringify(updItem),
+        // })
 
-        const data = await response.json()
+        // const data = await response.json()
 
 
-        setFeedback(feedback.map((item) => (item.id === id ? data : item)))
+        setFeedback(feedback.map((item) => (item.id === id ? {...item, ...updItem} : item)))
 
         setFeedbackEdit({
             item: {},
@@ -81,7 +96,7 @@ export const FeedbackProvider = ({ children }) => {
             value={{
                 feedback,
                 feedbackEdit,
-                isLoading,
+                // isLoading,
                 deleteFeedback,
                 addFeedback,
                 editFeedback,
